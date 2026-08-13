82446b17ce20f4b3f215aeedcee13c799d70b947
Chirag Wadehra <s4014390@student.rmit.edu.au>
2026-08-13 23:35:12 +1000

    feat(team-page): Update team member descriptions for 4/5 members

--- frontend/src/teamDescriptions.ts
+++ frontend/src/teamDescriptions.ts
@@ -7,7 +7,7 @@ export const teams: Team[] = [
     role: 'PROJECT MANAGER',
     initials: 'SZ',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
     boxColour: 'bg-blue-100',
     textColour: 'text-blue-800',
   },
@@ -16,7 +16,7 @@ export const teams: Team[] = [
     role: 'BUSINESS ANALYST',
     initials: 'AR',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      "Ahmed Falulur Rahuman is the BA on Team 2 and is in his final semester of a Bachelor of Computer Science at RMIT, majoring in Advanced Computer Science. In this project, he is responsible for gathering and documenting business requirements, working with the team to ensure the project aligns with the client’s needs, and contributing to the overall project planning. Outside of university, he plays football semi-competitively.",
     boxColour: 'bg-green-100',
     textColour: 'text-green-800',
   },
@@ -25,7 +25,7 @@ export const teams: Team[] = [
     role: 'UX Designer',
     initials: 'ZC',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      "Zac Clarkson is the UX Designer on Team 2, in his final semester of a Bachelor of Computer Science at RMIT (Enterprise Systems Development). On this project he owns all the research including participant and online. Outside uni he's VP of the RMIT Board Games Association and maintains a self-hosted infrastructure stack.",
     boxColour: 'bg-yellow-100',
     textColour: 'text-amber-800',
   },
@@ -34,7 +34,7 @@ export const teams: Team[] = [
     role: 'DEVELOPER',
     initials: 'ZH',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.',
+      "Hey there, I'm Zafir! I'm one of the developers for this team. In my free time, I like programming and reading. I'm a jack of all trades when it comes to programming. I've done graphics programming (SDL and raylib), tooling development (interpreters, language theory), high-performance computing (matrix math libraries), and general low-level work.",
     boxColour: 'bg-pink-100',
     textColour: 'text-pink-800',
   },
@@ -43,7 +43,7 @@ export const teams: Team[] = [
     role: 'DEVELOPER',
     initials: 'CW',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      "Chirag Wadehra is working as a Developer on Team 2. He is currently in the final semester of his Bachelor of Computer Science at RMIT University, majoring in Advanced Computer Science. As a Developer, he is responsible for the development aspects of the project, including implementing designs created by the UX designer and developing technical solutions to requirements and problems identified by the Project Manager. Outside of his coursework, Chirag has an interest in stock market trading and enjoys playing badminton.",
     boxColour: 'bg-green-100',
     textColour: 'text-green-800',
   }
